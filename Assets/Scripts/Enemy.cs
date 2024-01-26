950b44aeae280302814f11a4a49f52f71d5fc975
Ati Marin <ati.marin1@gmail.com>
2024-01-27 00:53:55 +0200

    navmesh stuff

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : Entity
+{
+    [SerializeField]
+    Transform _target;
+
+    [SerializeField]
+    bool _isEnabled = true;
+
+    private NavMeshAgent _agent;
+
+    private void Awake()
+    {
+        _agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if(_isEnabled)
+        {
+            // if(_agent.destination != _target.position)
+            _agent.SetDestination(_target.position);
+        }
+    }
+}
