8e4d92dc707e7565f78b6f52061c770ffdbc3427
Ati Marin <ati.marin1@gmail.com>
2024-01-28 15:47:29 +0200

    boss particle stuff

---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    public bool isEnabled = true;
+
+    private Transform playerPos;
+
+    private void Start()
+    {
+        playerPos = FindFirstObjectByType<FPSController>().transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnabled)
+        {
+            transform.LookAt(playerPos);
+        }
+    }
+}
